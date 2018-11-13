8f603ffc38bc1d9bedd9c91e23df7fccdf8241b5
september <zzzhen1994@gmail.com>
2018-11-13 11:04:36 +0800

    feat:改maven plugin为命令行输入

--- src/main/java/com/enzhico/Main.java
+++ src/main/java/com/enzhico/Main.java
@@ -1,15 +1,19 @@
 package com.enzhico;
 
 
-import io.github.swagger2markup.GroupBy;
 import io.github.swagger2markup.Language;
 import io.github.swagger2markup.Swagger2MarkupConfig;
 import io.github.swagger2markup.Swagger2MarkupConverter;
 import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
 import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.asciidoctor.*;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 
 /**
@@ -17,22 +21,48 @@ import java.nio.file.Paths;
  */
 public class Main {
 
-    private static final long serialVersionUID = 1L;
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("need input file and output file");
+            System.out.println("example: ./swagger.yml ./doc.html");
+            System.exit(-1);
+        }
+        String formFilePath = args[0];
+        String toFilePath = args[1];
 
-    public static void main(String[] args) throws Exception {
-        Path localSwaggerFile = Paths.get("src/main/resources/swagger.yaml");
-        Path outputFile = Paths.get("build/swagger");
+        System.out.printf("convert %s to %s \n", formFilePath, toFilePath);
+        Path fromFile = null;
+        Path toFile = null;
+        try {
+            fromFile = Paths.get(formFilePath);
+            toFile = Paths.get(toFilePath);
+        } catch (InvalidPathException e) {
+            System.out.println("invalid arguments,please input correct arguments");
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+        try {
+            Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                    .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                    .withOutputLanguage(Language.ZH)
+                    .withGeneratedExamples()
+                    .withoutInlineSchema()
+                    .build();
+            Swagger2MarkupConverter converter = Swagger2MarkupConverter.from(fromFile)
+                    .withConfig(config)
+                    .build();
 
-        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
-                .withOutputLanguage(Language.ZH)
-                .withPathsGroupedBy(GroupBy.TAGS)
-                .withGeneratedExamples()
-                .withoutInlineSchema()
-                .build();
-        Swagger2MarkupConverter converter = Swagger2MarkupConverter.from(localSwaggerFile)
-                .withConfig(config)
-                .build();
-        converter.toFile(outputFile);
+            Attributes attributes = AttributesBuilder.attributes().dataUri(true).get();
+
+            OptionsBuilder optionsBuilder = OptionsBuilder.options().headerFooter(true).safe(SafeMode.UNSAFE).backend("html5").attributes(attributes);
+            Map<String, Object> m = optionsBuilder.asMap();
+            Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+            String html = asciidoctor.convert(converter.toString(), m);
+            Files.write(toFile, html.getBytes(Charset.forName("utf-8")));
+            System.out.println("success");
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+            System.exit(-1);
+        }
     }
 }
